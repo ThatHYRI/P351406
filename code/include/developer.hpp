6781d679249150cffc191b9ba721ed85819a9cd5
MatthiesOtten <156666223+MatthiesOtten@users.noreply.github.com>
2024-06-06 00:52:41 +0200

    Added Developer.hpp + short summary

---
+++ code/include/developer.hpp
@@ -0,0 +1,20 @@
+//0 add class Developer as basis for JuniorDev and SeniorDev (inheritance). Things below should be in class developer.
+
+//1 Name
+
+//2 Alias
+
+//3 Logo
+
+//4 Constructor for 2 persons with name and alias. The logo should be outside of the constructor
+
+//5 Static method drink_coffee with Output:"Gimme that caffeine"
+
+//6 Save logo in membervariable "load_logo_from_file"
+
+//7 Wrong Name -> runtime_error_Exception 
+//6+7 Add text with compiler definition to CMakeLists.txt
+
+//8 getter-method for member
+
+//9 virtual-method "solve_problem"
